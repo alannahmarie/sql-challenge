ced53945fb3bb025596e328554587fd5417a0e3a
Alannah Fleet <alannahmarie@Alannahs-MBP.fios-router.home>
2021-03-04 10:43:16 -0500

    Queries complete

--- EmployeeSQL/submission/queries.sql
+++ EmployeeSQL/submission/queries.sql
@@ -5,12 +5,10 @@ FROM employees
 LEFT JOIN salaries
 ON employees.emp_no = salaries.emp_no;
 
-
 -- List first name, last name, and hire date for employees who were hired in 1986.
 SELECT employees.first_name, employees.last_name, employees.hire_date
 FROM employees
 WHERE hire_date BETWEEN '1984-01-01' AND '1984-12-31';
-
 
 -- List the manager of each department with the following information: 
 -- department number, department name, the manager's employee number, last name, first name.
@@ -18,7 +16,6 @@ SELECT departments.dept_no, departments.dept_name, employees.emp_no, employees.l
 FROM departments
 JOIN dept_manager ON (departments.dept_no = dept_manager.dept_no)
 JOIN employees ON (employees.emp_no = dept_manager.emp_no);
-
 
 -- List the department of each employee with the following information: 
 -- employee number, last name, first name, and department name.
