7249426b4423fbf860be9fc23a9ceb593eb5b68d
Alannah Fleet <alannahmarie@Alannahs-MBP.fios-router.home>
2021-03-04 00:22:30 -0500

    Updated schema

--- EmployeeSQL/submission/table_schemata.sql
+++ EmployeeSQL/submission/table_schemata.sql
@@ -1,8 +1,8 @@
-DROP TABLE dept_emp;
+DROP TABLE 
 
 -- Create table 1
 CREATE TABLE departments (
-	dept_no VARCHAR,
+	dept_no VARCHAR(4) PRIMARY KEY,
 	dept_name VARCHAR
 );
 
@@ -10,42 +10,60 @@ CREATE TABLE departments (
 SELECT * FROM departments;
 
 -- Create table 2
-CREATE TABLE dept_emp (
-	emp_no VARCHAR,
-	dept_no VARCHAR
-);
-
--- View table columns and datatypes
-SELECT * FROM dept_emp;
-
--- Create table 3
-CREATE TABLE dept_manager (
-	emp_no VARCHAR,
-	dept_no VARCHAR
-);
-
--- View table columns and datatypes
-SELECT * FROM dept_manager;
-
--- Create table 4
-CREATE TABLE employees (
-	emp_no INT,
-	emp_title_id VARCHAR,
-	birth_date VARCHAR, 
-	first_name VARCHAR,
-	last_name VARCHAR, 
-	sex VARCHAR,
-	hire_date VARCHAR
-);
-
--- View table columns and datatypes
-SELECT * FROM employees;
-
--- Create table 6
 CREATE TABLE titles (
-	title_id VARCHAR,
+	title_id VARCHAR(5) PRIMARY KEY,
 	title VARCHAR
 );
 
 -- View table columns and datatypes
 SELECT * FROM titles;
+
+-- Create table 3
+CREATE TABLE employees (
+	emp_no INT PRIMARY KEY,
+	emp_title_id VARCHAR,
+	birth_date VARCHAR, 
+	first_name VARCHAR,
+	last_name VARCHAR, 
+	sex VARCHAR,
+	hire_date DATE,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+-- View table columns and datatypes
+SELECT * FROM employees;
+
+-- Create table 4
+CREATE TABLE dept_emp (
+	emp_no INT,
+	dept_no VARCHAR(4),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY(emp_no, dept_no)
+);
+
+-- View table columns and datatypes
+SELECT * FROM dept_emp;
+
+-- Create table 5
+CREATE TABLE dept_manager (
+	emp_no INT,
+	dept_no VARCHAR,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY(emp_no, dept_no)
+);
+
+-- View table columns and datatypes
+SELECT * FROM dept_manager;
+
+-- Create table 6
+CREATE TABLE salaries (
+	emp_no INT,
+	salary VARCHAR,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY(emp_no, salary)
+);
+
+-- View table columns and datatypes
+SELECT * FROM salaries;
